269875c240fe2a5359305eef697a663be2d23355
Max Le <gop_max@m1.com.sg>
2024-04-04 10:15:45 +1100

    add type

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ Basic async coroutine
 import asyncio
 import random
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous coroutine that waits for a
     random amount of time between 0 and max_delay seconds.
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,9 +3,10 @@
 Concurrent coroutines
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns wait_random n times with the specified max_delay.
     """
