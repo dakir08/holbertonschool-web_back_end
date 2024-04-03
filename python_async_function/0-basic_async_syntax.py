1f6e31869fd7b8b2442e58f1df67776f263f64aa
Max Le <gop_max@m1.com.sg>
2024-04-04 10:21:34 +1100

    2

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -11,9 +11,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Asynchronous coroutine that waits for a
     random amount of time between 0 and max_delay seconds.
     """
-    # Generate a random delay
     delay = random.uniform(0, max_delay)
-    # Wait for the generated delay
     await asyncio.sleep(delay)
-    # Return the delay
     return delay
---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Measure runtime
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures and returns the average execution time
+    of calling wait_n with the provided arguments.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
