e5d11629a8230a9885ab5a045e7e5bf5df865e64
Max Le <gop_max@m1.com.sg>
2024-04-04 10:57:04 +1100

    0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Concurrent coroutines
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    An asynchronous generator that yields a random number
+    between 0 and 10 every 1 second,
+    for a total of 10 times.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)  # Asynchronously wait for 1 second
+        yield random.uniform(0, 10)  # Yield a random float between 0 and 10
---
+++ python_async_comprehension/README.md
@@ -0,0 +1,12 @@
+Resources
+Read or watch:
+
+PEP 530 – Asynchronous Comprehensions
+What’s New in Python: Asynchronous Comprehensions / Generators
+Type-hints for generators
+
+Learning Objectives
+
+How to write an asynchronous generator
+How to use async comprehensions
+How to type-annotate generators
