2db2b7e3d4e6d20a1a5e344d6de81dd31e3f9823
Max Le <gop_max@m1.com.sg>
2024-04-04 10:29:52 +1100

    add task

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,7 +2,7 @@ import asyncio
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Creates an asyncio.Task that waits for a random amount of time
     up to max_delay seconds.
