d8e9d33f5c600bc4ffc0957527415905a1cc4789
Max Le <gop_max@m1.com.sg>
2024-04-04 11:50:27 +1100

    sort order

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,8 +3,8 @@
 Async Comprehensions
 """
 
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
