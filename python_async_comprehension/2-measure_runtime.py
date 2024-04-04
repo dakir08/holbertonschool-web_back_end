7cbba354a2d811c16133a446484c7b202c9d611c
Max Le <gop_max@m1.com.sg>
2024-04-04 11:51:38 +1100

    reformt

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -5,8 +5,6 @@ Run time for four parallel comprehensions
 
 import asyncio
 import time
-
-
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
@@ -16,6 +14,9 @@ async def measure_runtime() -> float:
     and measures the total runtime.
     """
     start_time = time.time()
-    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
     end_time = time.time()
     return end_time - start_time
