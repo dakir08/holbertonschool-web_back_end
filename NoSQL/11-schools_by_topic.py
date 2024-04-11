909eca1f3c8177e1e898c181a7446091bc025cc5
Max Le <gop_max@m1.com.sg>
2024-04-11 12:45:25 +1000

    11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns the list of school having a specific topic
+    """
+    documents = mongo_collection.find({"topics": topic})
+    list_docs = [document for document in documents]
+    return list_docs
