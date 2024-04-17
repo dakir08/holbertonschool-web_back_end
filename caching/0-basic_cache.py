6b43864fa2efbdbe274e6351895c3dcf3879b980
Max Le <gop_max@m1.com.sg>
2024-04-17 15:24:56 +1000

    0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -11,10 +11,16 @@ class BasicCache(BaseCaching):
     BasicCache class
     """
     def put(self, key, item):
+        """
+        Save item
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Get item
+        """
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
