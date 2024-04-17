bc8976c4d4d8a05a3c6c760c1b9dd9adc51b22a0
Max Le <gop_max@m1.com.sg>
2024-04-17 15:28:29 +1000

    1

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+BasicCache class
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class
+    """
+    def __init__(self):
+        """
+        constructor
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        Save item
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print(f'DISCARD: {discard}')
+
+    def get(self, key):
+        """
+        Get item
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
