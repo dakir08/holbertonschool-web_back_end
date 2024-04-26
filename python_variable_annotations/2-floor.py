be8b74c2a3212b2d911f33e8fed13f4e30dc0e23
Max Le <gop_max@m1.com.sg>
2024-04-26 15:29:18 +1000

    2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+return floor
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    ''' Return largest int value less than or equal to n. '''
+    return math.floor(n)
