6a2d6f5a4b4d3c2379d77d2152c73c37f0a8a9c7
Max Le <gop_max@m1.com.sg>
2024-04-26 16:29:05 +1000

    6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+6-sum_mixed_list.py
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    return sum of mixed list
+    """
+    return sum(mxd_lst)
