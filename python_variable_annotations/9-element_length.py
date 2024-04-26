8c52f482c1572e62419c45116d255183b28fac6e
Max Le <gop_max@m1.com.sg>
2024-04-26 16:37:10 +1000

    9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Annotate the below function’s parameters
+and return values with the appropriate types
+"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(list: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return tuples
+    """
+    return [(i, len(i)) for i in list]
